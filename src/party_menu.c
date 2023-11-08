5b155188bbfdc4ad50a73ca63cc762bccfbf5c07
lbsbezerra <lbsbezerra@gmail.com>
2023-11-08 16:30:20 +0100

    Couldn't implement DexNav yet (will check in the future). For now, everything up to Fallarbor finished! Now going to take care of Route 114 rocky mountains. Also implemented Pokabbie Emerald Rogue option to 'press B to run' from wild battles, and reusable TMs, similar to modern pokemon games

--- src/party_menu.c
+++ src/party_menu.c
@@ -5101,8 +5101,6 @@ static void Task_LearnedMove(u8 taskId)
     if (move[1] == 0)
     {
         AdjustFriendship(mon, FRIENDSHIP_EVENT_LEARN_TMHM);
-        if (!ItemId_GetImportance(item))
-            RemoveBagItem(item, 1);
     }
     GetMonNickname(mon, gStringVar1);
     StringCopy(gStringVar2, gMoveNames[move[0]]);
